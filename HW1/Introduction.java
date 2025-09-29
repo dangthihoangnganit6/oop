f24437d29c3d090b240f50c29ece42cf2b084735
dangthihoangnganit6 <24020249@vnu.edu.vn>
2025-09-29 17:15:40 +0700

    Add Introduction.java in HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,11 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.print("Đặng Thị Hoàng Ngân" + "\t" + "24020249" + "\t" + "K69I-IT6");
+        System.out.println("\t" + "dangthihoangnganit6" + "\t" + "24020249@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.\n"
+                    + "Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
